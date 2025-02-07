8de0190d36a8bd9125ed2040867d27230f0e34fd
immohitk <mohitkumar9785@outlook.com>
2025-02-08 01:29:14 +0530

    String chapter added

---
+++ 07.strings.js
@@ -0,0 +1,7 @@
+// Strings in JavaScript
+
+// Strings are a sequence of characters. They can be enclosed in single quotes or double quotes.
+
+
+let str = "Hello, World!";
+let str2 = 'Hello, World!';
