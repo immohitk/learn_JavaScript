d6ac53ae4f0f93ea3eb814ac593d4da6b5c259d9
immohitk <mohitkumar9785@outlook.com>
2025-02-09 06:24:21 +0530

    dom added

---
+++ 10.DOM.js
@@ -0,0 +1,25 @@
+/*
+DOM (Document Object Model)
+
+- DOM is a programming interface for HTML and XML documents. It represents the structure of a document as a tree of nodes. Each node in the tree represents an element in the document.
+
+*/
+
+/*
+Window Object
+
+- The window onject represents an open window in a browser. It is browser's object(not JavaScript's) and is automatically created by browser.
+
+- It is a global object with lots of properties and methods. It is used to interact with the browser and the document.
+
+*/
+
+/*
+Dom manipulation
+
+- DOM manipulation is the process of changing the structure of a document by adding, removing, or modifying elements.
+
+- It is a crucial part of web development, as it allows developers to dynamically update the content of a web page.
+
+
+*/
